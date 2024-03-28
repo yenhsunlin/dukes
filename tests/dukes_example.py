213b801fe58c23d418074a514e11ec79ee1088a9
Yen-Hsun Lin <38074446+yenhsunlin@users.noreply.github.com>
2024-03-28 22:15:19 +0800

    Update multiple files

--- tests/dukes_example.py
+++ tests/dukes_example.py
@@ -1,13 +1,13 @@
-# duke_example.py
+# dukes_example.py
 
 import sys
-import duke
+import dukes
 
 if __name__ == '__main__':
 
     #Tx,mx = 5,1
     Tx = int(sys.argv[1])   # DM kinetic energy, MeV
     mx = int(sys.argv[2])   # DM mass, MeV
-    vx = duke.vBDM(Tx,mx)   # BDM velocity
+    vx = dukes.vBDM(Tx,mx)   # BDM velocity
     
     print(vx)               # Print the BDM velocity
