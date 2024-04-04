201e781b6d75ac3886f386a33d3ea7cd128f62ae
Yen-Hsun Lin <38074446+yenhsunlin@users.noreply.github.com>
2024-04-05 00:32:16 +0800

    Update cosmicAgeFit.py

--- src/dukes/dat/cosmicAgeFit.py
+++ src/dukes/dat/cosmicAgeFit.py
@@ -1,3 +1,20 @@
+# Created by Yen-Hsun Lin (Academia Sinica) in 04/2024.
+# Copyright (c) 2024 Yen-Hsun Lin.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version (see <http://www.gnu.org/licenses/>).
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+
+
+# Data points evaluated from https://www.astro.ucla.edu/~wright/CosmoCalc.html
+
 _z_data = [0.0,0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5,7.0,7.5,8.0,8.5,9.0]
 
 # Gyr
